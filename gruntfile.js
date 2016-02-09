7256d2732b63ef7bba23e9a572dc5d15621db4b6
goodbedford <bedford@goodbedford.com>
2016-02-08 20:35:17 -0800

    updated dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,8 @@
-module.exports = function(grunt) {
-    "use strict";
+(function() {
+    module.exports = function(grunt) {
+        "use strict";
 
-    require("load-grunt-tasks")(grunt);
+        require("load-grunt-tasks")(grunt);
 
-};
+    };
+})();
