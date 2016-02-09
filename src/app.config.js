bd94797de3764a971a656e7e09f3070252b24efc
goodbedford <bedford@goodbedford.com>
2016-02-08 22:29:47 -0800

    updated app config added home

---
+++ src/app.config.js
@@ -0,0 +1,28 @@
+(function() {
+    "use strict";
+
+    angular
+        .module("gbBlog")
+        .config(config);
+
+    config.$inject = ["$locationProvider", "$stateProvider", "$urlRouterProvider"];
+
+    function config($locationProvider, $stateProvider,$urlRouterProvider  ) {
+
+        $stateProvider
+            .state("home", {
+                url: "/",
+                controller: "HomeController",
+                controllerAs: "home",
+                templateUrl: "layout/home.tpl.html"
+            });
+        $urlRouterProvider.otherwise("/");
+
+        //remove hash in urls
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: true
+        });
+    }
+
+})();
